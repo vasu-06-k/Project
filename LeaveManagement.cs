3d27ed2bc98e072248566e3a9f182e7e3aed5535
Shreeraksha-S <91690777+Shreeraksha-S@users.noreply.github.com>
2021-11-15 17:57:02 +0530

    BAL

---
+++ LeaveManagement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRBAL1
+{
+    class LeaveManagement
+    {
+        public int EmpId { get; set; }
+        public int TotalLeave { get; set; }
+        public int BalanceLeave { get; set; }
+        public int CasualLeave { get; set; }
+        public int EarnedLeave { get; set; }
+        public int UnpaidLeave { get; set; }
+    }
+}
---
+++ PayRoll.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRBAL1
+{
+    class PayRoll
+    {
+        public int EmpId { get; set; }
+        public int BankAccountNo { get; set; }
+        public int ProvidentFundNo { get; set; }
+        public int BasicPay { get; set; }
+        public int HRA { get; set; }
+        public int MedicalAllowance { get; set; }
+        public int Tax { get; set; }
+        public int Bonus { get; set; }
+        public int GrossPay { get; set; }
+        public int NoOfWorkDays { get; set; }
+    }
+}
