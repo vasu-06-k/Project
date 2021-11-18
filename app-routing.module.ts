a0384c2f86bd5e949ce581fe55e1335a4b3be683
Shreeraksha-S <91690777+Shreeraksha-S@users.noreply.github.com>
2021-11-18 23:07:58 +0530

    Add files via upload

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -1,9 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddemployeeComponent } from './addemployee/addemployee.component';
+import { AddprojectComponent } from './addproject/addproject.component';
+import { AddresumeComponent } from './addresume/addresume.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ShowprojectsComponent } from './showprojects/showprojects.component';
+import { WrongUserComponent } from './wrong-user/wrong-user.component';
+ShowprojectsComponent
 
 const routes: Routes = [
-  {path:'LoginPage',component:LoginComponent}
+  {path:'LoginPage',component:LoginComponent},
+  {path:'wrong-user',component:WrongUserComponent},
+  {path:'home',component:HomeComponent},
+  {path:'Employee-Details',component:EmployeeDetailsComponent},
+  {path:'addemployee',component:AddemployeeComponent},
+  {path:'addresume',component:AddresumeComponent},
+  {path:'viewallresume',component:ResumeComponent},
+  {path:'addproject',component:AddprojectComponent},
+  {path:'viewallprojects',component:ShowprojectsComponent},
 ];
 export default routes;
 @NgModule({
--- app.module.ts
+++ app.module.ts
@@ -2,18 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import routes,{ AppRoutingModule } from './app-routing.module';
 import { MyloginService } from './mylogin.service';
+
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-
+import { FormsModule } from '@angular/forms';
+import { WrongUserComponent } from './wrong-user/wrong-user.component';
+import { HomeComponent } from './home/home.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { AddemployeeComponent } from './addemployee/addemployee.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ResumeService } from './resume.service';
+import { AddresumeComponent } from './addresume/addresume.component';
+import { AddprojectComponent } from './addproject/addproject.component';
+import { ShowprojectsComponent } from './showprojects/showprojects.component';
+import { ProjectService } from './project.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    WrongUserComponent,
+    HomeComponent,
+    EmployeeDetailsComponent,
+    AddemployeeComponent,
+    ResumeComponent,
+    AddresumeComponent,
+    AddprojectComponent,
+    ShowprojectsComponent
 
   ],
   imports: [
@@ -24,7 +43,7 @@ import { LoginComponent } from './login/login.component';
     FormsModule
    
   ],
-  providers: [MyloginService],
+  providers: [ProjectService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
